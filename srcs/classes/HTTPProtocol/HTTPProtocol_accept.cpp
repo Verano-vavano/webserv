23ca47e4b7fecd4f6ac5a18b012e0b4aa5c07b6d
Hugo Dupire <hugod1383@gmail.com>
2024-03-22 14:48:39 +0100

    Accept architecture (with q=) and parsing added
    406 errors doesnt work anymore idk y but no biggie. Will correct it as soon as I can charge my laptop lmao. Accept works pretty darn well. With a ; separator and a hierarchy of admitted filetypes. We just have to add equivalent typings

--- srcs/classes/HTTPProtocol/HTTPProtocol_accept.cpp
+++ srcs/classes/HTTPProtocol/HTTPProtocol_accept.cpp
@@ -1,13 +1,45 @@
 #include "HTTPProtocol.hpp"
 
 void	HTTPProtocol::check_type(t_response_creator &r) {
+	HTTPProtocol::print_request(r.req);
 	std::map<std::string, std::vector<std::string> >::iterator	accept = r.req.headers.find("Accept");
 	if (accept == r.req.headers.end()) { return ; }
 
 	std::vector<std::string>	values = accept->second;
+	std::pair<std::string, float>	typing;
+	typing.first = "NONE";
+	typing.second = 0;
+
+	std::string			type, q_str;
+	float				q;
+	unsigned long			coma_sep;
 	for (std::vector<std::string>::const_iterator it = values.begin(); it != values.end(); it++) {
-		if (r.file_type == *it || it->substr(0, 1) == "*" || it->substr(0, 3) == "*/*") { return ; }
+		coma_sep = it->find(";");
+		if (coma_sep > it->size()) {
+			type = *it;
+			q = 1.f;
+		} else {
+			type = it->substr(0, coma_sep);
+			q_str = it->substr(coma_sep + 1);
+			if (q_str.size() < 3 || q_str.size() > 10 || q_str.substr(0, 2) != "q=") { q = 1.f; }
+			else {
+				q_str = q_str.substr(2);
+				q = atof(q_str.c_str());
+				if (q < 0) { q = 0; }
+				else if (q > 1) {q = 1; }
+			}
+		}
+		if (r.file_type == type || is_wildcard_match(r.file_type, type)) {
+			if (q > typing.second) {
+				typing.first = r.file_type;
+			}
+			if (q == 1) { break ; }
+		}
 	}
-	r.err_code = 406;
+	if (typing.first == "NONE") {
+		r.err_code = 406;
+	} else {
+		r.file_type = typing.first;
+	}
 	return ;
 }
