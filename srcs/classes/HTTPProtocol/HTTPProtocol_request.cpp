c4c959f6953b18bad9b38abd603eb742b830d49d
Hugo Dupire <hdupire@student.42nice.fr>
2024-04-17 13:16:41 +0200

    Fixed bug when POST and header size too big
    If header size too big, the body would be fully read, and thus leading to an infinite loop

--- srcs/classes/HTTPProtocol/HTTPProtocol_request.cpp
+++ srcs/classes/HTTPProtocol/HTTPProtocol_request.cpp
@@ -119,10 +119,13 @@ int	HTTPProtocol::read_and_understand_request(int fd, t_response_creator &r) {
 	std::map<std::string, std::vector<std::string> >::const_iterator	finder = r.req.headers.find("content-length");
 	if (finder == r.req.headers.end()) { r.err_code = 411; this->empty_fd_in(fd); return (1); } // No Content-Length header (required for body length)
 
-	// BODY reading
-	ret = this->read_crlfcrlf(fd, r, r.conf->client_body_buffer_size, request, atol(finder->second[0].c_str()));
+	long	length = atol(finder->second[0].c_str()) -  request.size();
 
-	if (ret == -1)
+	// BODY reading
+	if (length != 0)
+		ret = this->read_crlfcrlf(fd, r, r.conf->client_body_buffer_size, request, length);
+
+	if (ret == -1 || length == 0)
 		r.req.body = request;
 	this->empty_fd_in(fd);
 
